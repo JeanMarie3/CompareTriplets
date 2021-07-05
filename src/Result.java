2e5f10dfb90e77e0abb61293c6af782c05f76db1
joyle1 <12914448+joyle1@users.noreply.github.com>
2021-07-05 06:27:17 +0200

    /* Java that works pointsAlice = ((a0>b0)?1:0)+ ((a1>b1)?1:0)+ ((a2>b2)?1:0) ;         pointsBob = ((a0<b0)?1:0)+ ((a1<b1)?1:0)+ ((a2<b2)?1:0) ;         System.out.println(pointsAlice+" " +pointsBob);         */

--- src/Result.java
+++ src/Result.java
@@ -40,6 +40,11 @@ class Result {
     }
 
 }
+ /* Java that works
+pointsAlice = ((a0>b0)?1:0)+ ((a1>b1)?1:0)+ ((a2>b2)?1:0) ;
+        pointsBob = ((a0<b0)?1:0)+ ((a1<b1)?1:0)+ ((a2<b2)?1:0) ;
+        System.out.println(pointsAlice+" " +pointsBob);
+
  /* C# that works
 
     int pointsAlice = ((a0>b0)?1:0)+ ((a1>b1)?1:0)+ ((a2>b2)?1:0);
