9ba14e85321ba96872211f01384adbae916c4bb5
joyle1 <12914448+joyle1@users.noreply.github.com>
2021-07-05 04:26:09 +0200

    Initial commit

---
+++ src/Result.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Result {
+
+    /*
+     * Complete the 'compareTriplets' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY a
+     *  2. INTEGER_ARRAY b
+     */
+
+    public static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
+        // Write your code here
+        List<Integer> score = new ArrayList<>();
+
+        int scoreAlice = 0;
+        int scoreBob = 0;
+        for (int i = 0; i < a.size(); i++){
+
+            if(a.get(i) > b.get(i)){
+
+                scoreAlice++;
+            }
+
+            else if(a.get(i) < b.get(i))
+            {
+                scoreBob++;
+            }
+
+        }
+
+        score.add(scoreAlice);
+        score.add(scoreBob);
+        return score;
+
+
+    }
+
+}
